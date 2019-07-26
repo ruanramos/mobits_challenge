a6aed12c428ffe71cf34afed93ed0fef658dec88
Ruan <ruan.ramos28@gmail.com>
2019-07-26 18:10:10 -0300

    big commit, methods and business rules, my bad.
    1- private constructor for encapsulation
    2- static method for calling constructor and making a withdrawal,
    cheking profile type and acting accordingly.
    3- method for checking if there is enough founds on the account
    (checkEnoughFounds).
    4- method for actually subtracting account balance (subtractBalance).
    5- method for applying tax to VIP account holder when negative balance
    (waitAndApplyTax).

--- src/Withdrawal.java
+++ src/Withdrawal.java
@@ -1,4 +1,93 @@
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
-public class Withdrawal {
+public class Withdrawal extends Transaction {
+
+	private Account account;
+	private BigDecimal tax;
+
+	
+	/**
+	 * Used a private constructor, instantiating the class with the static method 
+	 * makeWithdrawal() for encapsulation
+	 */
+	private Withdrawal(LocalDate date, LocalTime time, BigDecimal value, String description, Account account) {
+		super(date, time, value, description);
+		this.account = account;
+		this.tax = new BigDecimal("0.1");
+	}
+
+	static Withdrawal makeWithdrawal(LocalDate date, LocalTime time, BigDecimal value, String description,
+			Account account) {
+		Withdrawal w = new Withdrawal(date, time, value, description, account);
+
+		String profileType = account.getProfileType();
+		BigDecimal balance = account.getBalance();
+		int balanceStatusAfter = w.checkBalanceAfterWithdrawal(balance, value);
+
+		if (profileType == "Normal") {
+			if (balanceStatusAfter >= 0) {
+				w.subtractBalance(account, value);
+			} else {
+				System.out.println(String.format("%s %o.",
+						"Error: Not enough balance for the operation on account number ", account.getAccountNumber()));
+			}
+		} else if (profileType == "VIP") {
+			if (balanceStatusAfter >= 0) {
+				w.subtractBalance(account, value);
+			} else {
+				BigDecimal currentBalance = account.getBalance();
+				while (currentBalance.compareTo(new BigDecimal("0")) < 0) {
+					w.waitAndApplyTax(1, currentBalance, w.getTax());
+				}
+			}
+		}
+		return w;
+	}
+
+	/**
+	 * compareTo returns 0 if equals, 1 if grater than value, -1 if lesser than
+	 * value
+	 */
+	private int checkBalanceAfterWithdrawal(BigDecimal balance, BigDecimal value) {
+		return balance.compareTo(value);
+	}
+
+	private void subtractBalance(Account account, BigDecimal value) {
+		try {
+			BigDecimal newBalance = account.getBalance().subtract(value);
+			account.setBalance(newBalance);
+			System.out.println("Operation successfully completed. New balance: " + newBalance);
+		} catch (Exception e) {
+			System.out.println("Error: " + e);
+		}
+	}
+
+	private void waitAndApplyTax(int minutes, BigDecimal balance, BigDecimal tax) {
+		try {
+			Thread.sleep(60 * 1000 * minutes);
+		} catch (InterruptedException ex) {
+			Thread.currentThread().interrupt();
+		}
+		BigDecimal currentBalance = account.getBalance();
+		account.setBalance(currentBalance.subtract(currentBalance.multiply(tax)));
+	}
+
+	public Account getAccount() {
+		return account;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+
+	public BigDecimal getTax() {
+		return tax;
+	}
+
+	public void setTax(BigDecimal tax) {
+		this.tax = tax;
+	}
 
 }
