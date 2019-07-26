7605a7c4587d8e392e9879df4836bd3832405b65
Ruan <ruan.ramos28@gmail.com>
2019-07-26 18:30:09 -0300

    add super constructor to Transfer class

--- src/Transfer.java
+++ src/Transfer.java
@@ -1,4 +1,12 @@
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Transfer extends Transaction{
+
+	public Transfer(LocalDate date, LocalTime time, BigDecimal value, String description) {
+		super(date, time, value, description);
+		
+	}
 
 }
