ef19475e67fbb0991863b50ebea8fc62cbc964e3
Ruan <ruan.ramos28@gmail.com>
2019-07-26 18:09:45 -0300

    add constructor of abstract class Transaction

---
+++ src/Transaction.java
@@ -0,0 +1,56 @@
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+/**
+ * This class will be the base class for any transaction, 
+ * such as Withdrawal or Transfer
+ */
+
+public abstract class Transaction {
+
+	private LocalDate date;
+	private LocalTime time;
+	private BigDecimal value;
+	private String description;
+
+
+	public Transaction(LocalDate date, LocalTime time, BigDecimal value, String description) {
+		this.date = date;
+		this.time = time;
+		this.value = value;
+		this.description = description;
+	}
+
+	public LocalTime getTime() {
+		return time;
+	}
+
+	public void setTime(LocalTime time) {
+		this.time = time;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public BigDecimal getValue() {
+		return value;
+	}
+
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
