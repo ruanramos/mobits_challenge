6216ae2c99bf74aa5fd9c006ec22399884b65a19
Ruan <ruan.ramos28@gmail.com>
2019-07-26 18:07:30 -0300

    add profile type to account, was needed for Withdrawal class methods

--- src/Account.java
+++ src/Account.java
@@ -1,9 +1,10 @@
 import java.math.BigDecimal;
+import java.util.ArrayList;
 
 public class Account {
 
 	private long accountNumber;
-
+	private String profileType;
 	/**
 	 * 
 	 * The decision of using BigDecimal is to avoid the inaccuracy of float and
@@ -11,6 +12,14 @@ public class Account {
 	 * 
 	 */
 	private BigDecimal balance;
+	private ArrayList<Transaction> transactions;
+
+	public Account(long accountNumber, String profileType, BigDecimal balance, ArrayList<Transaction> transactions) {
+		this.accountNumber = accountNumber;
+		this.setProfileType(profileType);
+		this.balance = balance;
+		this.transactions = transactions;
+	}
 
 	public BigDecimal getBalance() {
 		return balance;
@@ -26,6 +35,22 @@ public class Account {
 
 	public void setAccountNumber(long accountNumber) {
 		this.accountNumber = accountNumber;
+	}
+
+	public ArrayList<Transaction> getTransactions() {
+		return transactions;
+	}
+
+	public void setTransactions(ArrayList<Transaction> transactions) {
+		this.transactions = transactions;
+	}
+
+	public String getProfileType() {
+		return profileType;
+	}
+
+	public void setProfileType(String profileType) {
+		this.profileType = profileType;
 	}
 
 }
