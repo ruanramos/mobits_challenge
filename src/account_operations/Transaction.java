5eff4b68421cdcbf6edd770d93e09ffd9b0bd38e
Ruan <ruan.ramos28@gmail.com>
2019-07-28 18:29:02 -0300

    change package name

---
+++ src/account_operations/Transaction.java
@@ -0,0 +1,99 @@
+package account_operations;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import account.Account;
+
+/**
+ * This class will be the base class for any transaction, such as Withdrawal or
+ * Transfer
+ */
+
+public abstract class Transaction {
+
+	private LocalDate date;
+	private LocalTime time;
+	private BigDecimal value;
+	private String description;
+
+	public Transaction(LocalDate date, LocalTime time, BigDecimal value, String description) {
+		this.date = date;
+		this.time = time;
+		this.value = value;
+		this.description = description;
+	}
+
+	/**
+	 * compareTo returns 0 if equals, 1 if grater than value, -1 if lesser than
+	 * value
+	 */
+	static boolean checkEnoughFounds(BigDecimal balance, BigDecimal value) {
+		int enoughFounds = balance.compareTo(value);
+		if (enoughFounds < 0)
+			return false;
+		return true;
+	}
+
+	static void subtractBalance(Account account, BigDecimal value) {
+		try {
+			BigDecimal newBalance = account.getBalance().subtract(value);
+			account.setBalance(newBalance);
+			System.out.println("Operation successfully completed. New balance: " + newBalance);
+		} catch (Exception e) {
+			System.out.println("Error: " + e);
+		}
+	}
+
+	static BigDecimal applyFixedTax(BigDecimal value, BigDecimal fixedTax) {
+		return value.add(fixedTax);
+	}
+
+	static BigDecimal applyPercentageTax(BigDecimal value, BigDecimal percentageTax) {
+		BigDecimal multiplier = new BigDecimal("1.0").add(percentageTax);
+		return value.multiply(multiplier);
+	}
+
+	static void addBalance(Account account, BigDecimal value) {
+		try {
+			BigDecimal newBalance = account.getBalance().add(value);
+			account.setBalance(newBalance);
+			System.out.println("Operation successfully completed. New balance: " + newBalance);
+		} catch (Exception e) {
+			System.out.println("Error: " + e);
+		}
+	}
+
+	public LocalTime getTime() {
+		return time;
+	}
+
+	public void setTime(LocalTime time) {
+		this.time = time;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public BigDecimal getValue() {
+		return value;
+	}
+
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
---
+++ src/account_operations/Withdrawal.java
@@ -0,0 +1,81 @@
+package account_operations;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import account.Account;
+
+public class Withdrawal extends Transaction {
+
+	// TODO treat concurrency problems
+	
+	private Account account;
+	private BigDecimal tax;
+
+	/**
+	 * Used a private constructor, instantiating the class with the static method
+	 * makeWithdrawal() for encapsulation
+	 */
+	private Withdrawal(LocalDate date, LocalTime time, BigDecimal value, String description, Account account) {
+		super(date, time, value, description);
+		this.account = account;
+		this.tax = new BigDecimal("0.1");
+	}
+
+	static Withdrawal makeWithdrawal(LocalDate date, LocalTime time, BigDecimal value, String description,
+			Account account) {
+		Withdrawal w = new Withdrawal(date, time, value, description, account);
+
+		String profileType = account.getProfileType();
+		BigDecimal balance = account.getBalance();
+		boolean enoughFounds = Transaction.checkEnoughFounds(balance, value);
+
+		if (profileType == "Normal") {
+			if (enoughFounds) {
+				Transaction.subtractBalance(account, value);
+			} else {
+				System.out.println(String.format("%s %o.",
+						"Error: Not enough balance for the operation on account number ", account.getAccountNumber()));
+			}
+		} else if (profileType == "VIP") {
+			if (enoughFounds) {
+				Transaction.subtractBalance(account, value);
+			} else {
+				BigDecimal currentBalance = account.getBalance();
+				while (currentBalance.compareTo(new BigDecimal("0")) < 0) {
+					w.waitAndApplyTax(1, currentBalance, w.getTax());
+				}
+			}
+		}
+		account.transactions.add(w);
+		return w;
+	}
+
+	private void waitAndApplyTax(int minutes, BigDecimal balance, BigDecimal tax) {
+		try {
+			Thread.sleep(60 * 1000 * minutes);
+		} catch (InterruptedException ex) {
+			Thread.currentThread().interrupt();
+		}
+		BigDecimal currentBalance = account.getBalance();
+		account.setBalance(currentBalance.subtract(currentBalance.multiply(tax)));
+	}
+
+	public Account getAccount() {
+		return account;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+
+	public BigDecimal getTax() {
+		return tax;
+	}
+
+	public void setTax(BigDecimal tax) {
+		this.tax = tax;
+	}
+
+}
