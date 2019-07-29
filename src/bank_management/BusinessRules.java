6c325b338865d318a56857840cfa1a9f3e2ad667
Ruan <ruan.ramos28@gmail.com>
2019-07-29 19:03:11 -0300

    add new package and class for managing bank information like taxes, maximum and minimum values and account related constants

---
+++ src/bank_management/BusinessRules.java
@@ -0,0 +1,58 @@
+package bank_management;
+
+import java.math.BigDecimal;
+
+public class BusinessRules {
+
+	/**
+	 * Account related
+	 */
+	private static final int lenAccountNumber = 8;
+	private static final int lenPassword = 6;
+
+	/**
+	 * Tax related
+	 */
+	private static final BigDecimal negativeBalanceTax = new BigDecimal("0.1");
+	private static final int taxApplicationInterval = 1;
+	private static final BigDecimal normalTransferTax = new BigDecimal("8");
+	private static final BigDecimal VipTransferTax = new BigDecimal("0.008");
+
+	/**
+	 * Transaction related
+	 */
+	private static final BigDecimal normalMaxTransferValue = new BigDecimal("1000");
+
+	
+	/**
+	 * Getters and Setters
+	 */
+	public static BigDecimal getNormaltransfertax() {
+		return normalTransferTax;
+	}
+
+	public static BigDecimal getViptransfertax() {
+		return VipTransferTax;
+	}
+
+	public static BigDecimal getNormalMaxtransfervalue() {
+		return normalMaxTransferValue;
+	}
+
+	public static int getLenaccountnumber() {
+		return lenAccountNumber;
+	}
+
+	public static int getLenpassword() {
+		return lenPassword;
+	}
+
+	public static BigDecimal getNegativebalancetax() {
+		return negativeBalanceTax;
+	}
+
+	public static int getTaxapplicationinterval() {
+		return taxApplicationInterval;
+	}
+
+}
