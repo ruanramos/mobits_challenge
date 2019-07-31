92a06ea69337da85fee0b6344eec6e049d34f634
Ruan <ruan.ramos28@gmail.com>
2019-07-31 04:25:03 -0300

    add method for querying all Accounts from bd

--- src/database/AccountStorage.java
+++ src/database/AccountStorage.java
@@ -3,7 +3,12 @@ package database;
 import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import account.Account;
 
 public class AccountStorage {
 	/**
@@ -26,9 +31,26 @@ public class AccountStorage {
 		}
 	}
 
-	public static void main(String[] args) {
-		AccountStorage test = new AccountStorage();
+	/**
+	 * select all rows in the Accounts table
+	 */
+	public ArrayList<Account> selectAllAccounts() {
+		String sql = "SELECT * FROM Accounts";
+		ArrayList<Account> accounts = new ArrayList<Account>();
 
-		test.insertAccount(2, BigDecimal.ZERO, 5);
+		try (Connection conn = SQLiteConnection.connect();
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(sql)) {
+
+			// loop through the result set
+			while (rs.next()) {
+				Account acc = new Account(rs.getLong("number"), rs.getInt("holder_id"), rs.getBigDecimal("balance"));
+				accounts.add(acc);
+			}
+			return accounts;
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return null;
 	}
 }
